89b74a25bb25064d40499db22bc21450676d1f20
sahimshisir <sahimshisir@gmail.com>
2024-09-16 02:44:36 +0600

    post design

--- src/components/frontend/pages/main.vue
+++ src/components/frontend/pages/main.vue
@@ -6,23 +6,86 @@
 
     <div class="row">
       <!-- Video Player -->
-      <div class="col-12 mb-4">
+      <div class="col-8 mb-4">
         <div class="card">
-          <div class="card-header d-flex">
-            <div class="avatar  me-3 ">
-                      <img src="../../../assets/frontend/assets/img/avatars/1.png"  class="h-auto rounded-circle" />
-                     
-                    </div>
-                    <div>
-                       <p class="mt-1">
-                      Alex leman
-                    </p>
-                    <span>10h ago</span>
-                    </div>
-                   
-                    
+          <div class="card-header">
+            <div class="d-flex">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div>
+                <h5>Alex leman</h5>
+                <span class="timer">10h ago</span>
+              </div>
+            </div>
+            <p class="mt-2 p-0">
+              lorem ipsum for caption Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit. Sapiente fugit incidunt inventore dolores
+              pariatur aut iusto aspernatur assumenda architecto consequatur
+              ratione earum id, sint labore officiis quo aperiam vero eligendi!
+            </p>
           </div>
-          
+
+          <div class="card-body">
+            <video
+              class="w-100"
+              poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
+              id="plyr-video-player"
+              playsinline
+              controls>
+              <source
+                src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
+                type="video/mp4" />
+            </video>
+          </div>
+          <div class="card-footer">
+            <div class="d-flex" style="justify-content:space-evenly;">
+              <button class="btn border-0">
+                <i class="fa-regular fa-thumbs-up me-1"></i> Like
+              </button>
+              <button
+                class="btn border-0"
+                data-bs-toggle="modal"
+                data-bs-target="#comment">
+                <i class="fa-regular fa-comment me-1"></i> Comment
+              </button>
+              <button class="btn border-0">
+                <i class="fa-regular fa-share-from-square me-1"></i>Share
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-4 mb-4">
+        <div id="my-card" style="position: fixed" class="card">
+          <div class="card-body">
+            <h4>Categories</h4>
+            <div class="border-bottom"></div>
+            <p>Web Development</p>
+            <p>Web Development</p>
+            <p>Web Development</p>
+            <p>Web Development</p>
+            <p>Web Development</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-8 mb-4">
+        <div class="card">
+          <h5 class="card-header">Video</h5>
+          <div class="card-body">
+            <img
+              class="w-100"
+              src="../../../assets/frontend/assets/img/backgrounds/6.jpg"
+              alt=""
+              srcset="" />
+          </div>
+        </div>
+      </div>
+      <div class="col-8 mb-4">
+        <div class="card">
+          <h5 class="card-header">Video</h5>
           <div class="card-body">
             <video
               class="w-100"
@@ -37,45 +100,27 @@
           </div>
         </div>
       </div>
-      <div class="col-12 mb-4">
+      <div class="col-8 mb-4">
         <div class="card">
           <h5 class="card-header">Video</h5>
           <div class="card-body">
-            <img class="w-100" src="../../../assets/frontend/assets/img/backgrounds/6.jpg" alt="" srcset="">
-           
+            <img
+              class="h-50 w-100"
+              src="../../../assets/frontend/assets/img/elements/1.jpg"
+              alt=""
+              srcset="" />
           </div>
         </div>
       </div>
-      <div class="col-12 mb-4">
+      <div class="col-8 mb-4">
         <div class="card">
           <h5 class="card-header">Video</h5>
           <div class="card-body">
-            <video
-              class="w-100"
-              poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
-              id="plyr-video-player"
-              playsinline
-              controls>
-              <source
-                src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
-                type="video/mp4" />
-            </video>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 mb-4">
-        <div class="card">
-          <h5 class="card-header">Video</h5>
-          <div class="card-body">
-            <img class="h-50 w-100" src="../../../assets/frontend/assets/img/elements/1.jpg" alt="" srcset="">
-          </div>
-        </div>
-      </div>
-      <div class="col-12 mb-4">
-        <div class="card">
-          <h5 class="card-header">Video</h5>
-          <div class="card-body">
-            <img class="h-50 w-100" src="../../../assets/frontend/assets/img/ecommerce-images/product-10.png" alt="" srcset="">
+            <img
+              class="h-50 w-100"
+              src="../../../assets/frontend/assets/img/ecommerce-images/product-10.png"
+              alt=""
+              srcset="" />
           </div>
         </div>
       </div>
@@ -97,5 +142,266 @@
       </div> -->
       <!-- /Audio Player -->
     </div>
+    <div class="modal fade" id="comment" tabindex="-1" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="modalScrollableTitle">Comments</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="d-flex mb-4">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div class="">
+                <h6>Alex leman</h6>
+                <div class="bg-primary p-3 comment  text-white rounded">
+                  <span>very nice and nice comment</span>
+                </div>
+                <div class="comment_reaction">
+                  <button class="btn border-0" style="cursor:text;">1h</button>
+                  <button class="btn border-0">Like</button>
+                  <button class="btn border-0">Reply</button>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex mb-4">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div class="">
+                <h6>Alex leman</h6>
+                <div class="bg-primary p-3 comment  text-white rounded">
+                  <span>very nice and nice comment</span>
+                </div>
+                <div class="comment_reaction">
+                  <button class="btn border-0" style="cursor:text;">1h</button>
+                  <button class="btn border-0">Like</button>
+                  <button class="btn border-0">Reply</button>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex mb-4">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div class="">
+                <h6>Alex leman</h6>
+                <div class="bg-primary p-3 comment  text-white rounded">
+                  <span>very nice and nice comment</span>
+                </div>
+                <div class="comment_reaction">
+                  <button class="btn border-0" style="cursor:text;">1h</button>
+                  <button class="btn border-0">Like</button>
+                  <button class="btn border-0">Reply</button>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex mb-4">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div class="">
+                <h6>Alex leman</h6>
+                <div class="bg-primary p-3 comment  text-white rounded">
+                  <span>very nice and nice comment</span>
+                </div>
+                <div class="comment_reaction">
+                  <button class="btn border-0" style="cursor:text;">1h</button>
+                  <button class="btn border-0">Like</button>
+                  <button class="btn border-0">Reply</button>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex mb-4">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div class="">
+                <h6>Alex leman</h6>
+                <div class="bg-primary p-3 comment  text-white rounded">
+                  <span>very nice and nice comment</span>
+                </div>
+                <div class="comment_reaction">
+                  <button class="btn border-0" style="cursor:text;">1h</button>
+                  <button class="btn border-0">Like</button>
+                  <button class="btn border-0">Reply</button>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex mb-4">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div class="">
+                <h6>Alex leman</h6>
+                <div class="bg-primary p-3 comment  text-white rounded">
+                  <span>very nice and nice comment</span>
+                </div>
+                <div class="comment_reaction">
+                  <button class="btn border-0" style="cursor:text;">1h</button>
+                  <button class="btn border-0">Like</button>
+                  <button class="btn border-0">Reply</button>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex mb-4">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div class="">
+                <h6>Alex leman</h6>
+                <div class="bg-primary p-3 comment  text-white rounded">
+                  <span>very nice and nice comment</span>
+                </div>
+                <div class="comment_reaction">
+                  <button class="btn border-0" style="cursor:text;">1h</button>
+                  <button class="btn border-0">Like</button>
+                  <button class="btn border-0">Reply</button>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex mb-4">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div class="">
+                <h6>Alex leman</h6>
+                <div class="bg-primary p-3 comment  text-white rounded">
+                  <span>very nice and nice comment</span>
+                </div>
+                <div class="comment_reaction">
+                  <button class="btn border-0" style="cursor:text;">1h</button>
+                  <button class="btn border-0">Like</button>
+                  <button class="btn border-0">Reply</button>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex mb-4">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div class="">
+                <h6>Alex leman</h6>
+                <div class="bg-primary p-3 comment  text-white rounded">
+                  <span>very nice and nice comment</span>
+                </div>
+                <div class="comment_reaction">
+                  <button class="btn border-0" style="cursor:text;">1h</button>
+                  <button class="btn border-0">Like</button>
+                  <button class="btn border-0">Reply</button>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex mb-4">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div class="">
+                <h6>Alex leman</h6>
+                <div class="bg-primary p-3 comment  text-white rounded">
+                  <span>very nice and nice comment</span>
+                </div>
+                <div class="comment_reaction">
+                  <button class="btn border-0" style="cursor:text;">1h</button>
+                  <button class="btn border-0">Like</button>
+                  <button class="btn border-0">Reply</button>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex mb-4">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div class="">
+                <h6>Alex leman</h6>
+                <div class="bg-primary p-3 comment  text-white rounded">
+                  <span>very nice and nice comment</span>
+                </div>
+                <div class="comment_reaction">
+                  <button class="btn border-0" style="cursor:text;">1h</button>
+                  <button class="btn border-0">Like</button>
+                  <button class="btn border-0">Reply</button>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex mb-4">
+              <div class="avatar me-3">
+                <img
+                  src="../../../assets/frontend/assets/img/avatars/1.png"
+                  class="h-auto rounded-circle" />
+              </div>
+              <div class="">
+                <h6>Alex leman</h6>
+                <div class="bg-primary p-3 comment  text-white rounded">
+                  <span>very nice and nice comment</span>
+                </div>
+                <div class="comment_reaction">
+                  <button class="btn border-0" style="cursor:text;">1h</button>
+                  <button class="btn border-0">Like</button>
+                  <button class="btn border-0">Reply</button>
+                </div>
+              </div>
+            </div>
+           
+           
+            
+          </div>
+         
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.timer {
+  display: block;
+  margin-top: -15px;
+  font-size: 10px;
+}
+.comment {
+  display: block;
+  margin-top: -10px;
+ 
+}
+.comment_reaction{
+  display: block;
+  margin-top: -4px;
+  
+}
+#my-card {
+  width: 24%;
+}
+.list-menu {
+  list-style-type: none;
+  line-height: 30px;
+}
+</style>
--- src/components/layout/Front.vue
+++ src/components/layout/Front.vue
@@ -1,66 +1,58 @@
 <script setup>
-import sidebar from '../frontend/include/sidebar.vue'
-import Header from '../frontend/include/topbar.vue'
-import Content from '../frontend/pages/main.vue'
+import sidebar from "../frontend/include/sidebar.vue";
+import Header from "../frontend/include/topbar.vue";
+import Content from "../frontend/pages/main.vue";
 // import footar from './components/frontend/include/footer.vue'
 </script>
 
 <template>
-   <div class="layout-wrapper layout-content-navbar">
-      <div class="layout-container">
-        <!-- Menu -->
-          <sidebar/>
-        <!-- / Menu -->
+  <div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+      <!-- Menu -->
+      <sidebar/>
+      <!-- / Menu -->
 
-        <!-- Layout container -->
-        <div class="layout-page">
-          <!-- Navbar -->
+      <!-- Layout container -->
+      <div class="layout-page">
+        <!-- Navbar -->
 
         <Header/>
 
-          <!-- / Navbar -->
+        <!-- / Navbar -->
 
-          <!-- Content wrapper -->
-          <div class="content-wrapper">
-            <!-- Content -->
-            <Content/>
-          
-            <!-- / Content -->
+        <!-- Content wrapper -->
+        <div class="content-wrapper">
+          <!-- Content -->
+          <Content/>
 
-            <!-- Footer -->
-           <!-- <footar/> -->
-            <!-- / Footer -->
+          <!-- / Content -->
 
-            <div class="content-backdrop fade"></div>
-          </div>
-          <!-- Content wrapper -->
+          <!-- Footer -->
+          <!-- <footar/> -->
+          <!-- / Footer -->
+
+          <div class="content-backdrop fade"></div>
         </div>
-        <!-- / Layout page -->
+        <!-- Content wrapper -->
       </div>
-
-      <!-- Overlay -->
-      <div class="layout-overlay layout-menu-toggle"></div>
-
-      <!-- Drag Target Area To SlideIn Menu On Small Screens -->
-      <div class="drag-target"></div>
+      <!-- / Layout page -->
+     
     </div>
+
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
+
+    <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+    <div class="drag-target"></div>
+  </div>
 </template>
 
 <script>
 export default {
-
   mounted() {
-    // Dynamically import the script when the component is mounted
-    import('../../assets/frontend/assets/js/dashboards-analytics.js'),
-    // import('./assets/frontend/assets/vendor/js/template-customizer.js'),
-    // import('./assets/frontend/assets/vendor/js/helpers.js'),
-    // import('./assets/frontend/assets/vendor/js/menu.js'),
-    import('../../assets/frontend/assets/js/main.js'),
-    import('../../assets/frontend/assets/js/extended-ui-media-player.js')
-
-     
+    import("../../assets/frontend/assets/js/dashboards-analytics.js"),
+      import("../../assets/frontend/assets/js/main.js"),
+      import("../../assets/frontend/assets/js/extended-ui-media-player.js");
   },
 };
 </script>
-
-

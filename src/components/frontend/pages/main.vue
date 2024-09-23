f4cb3a427a83cacc925e566d21cd7a14d478668c
Ashraful-Islam-sahim <ashrafulislamsahim@gmail.com>
2024-09-23 01:29:36 -0700

    post

--- src/components/frontend/pages/main.vue
+++ src/components/frontend/pages/main.vue
@@ -11,9 +11,7 @@
           <div class="card-header">
             <div class="d-flex">
               <div class="avatar me-3">
-                <img
-                  src="../../../assets/frontend/assets/img/avatars/1.png"
-                  class="h-auto rounded-circle" />
+                <img src="../../../assets/frontend/assets/img/avatars/1.png" class="h-auto rounded-circle" />
               </div>
               <div>
                 <h5>Alex leman</h5>
@@ -29,15 +27,9 @@
           </div>
 
           <div class="card-body">
-            <video
-              class="w-100 rounded"
-              poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
-              id="plyr-video-player"
-              playsinline
-              controls>
-              <source
-                src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
-                type="video/mp4" />
+            <video class="w-100 rounded" poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
+              id="plyr-video-player" playsinline controls>
+              <source src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4" type="video/mp4" />
             </video>
           </div>
           <div class="card-footer">
@@ -46,10 +38,7 @@
                 <i class="fa-regular fa-heart me-1 fs-4"></i>
                 <span style="font-size: 12px">12k</span>
               </button>
-              <button
-                class="btn border-0"
-                data-bs-toggle="modal"
-                data-bs-target="#comment">
+              <button class="btn border-0" data-bs-toggle="modal" data-bs-target="#comment">
                 <i class="fa-regular fa-comment me-1 fs-4"></i>
                 <span style="font-size: 12px">12k</span>
               </button>
@@ -96,10 +85,7 @@
         <div class="card">
           <h5 class="card-header">Video</h5>
           <div class="card-body">
-            <img
-              class="w-100"
-              src="../../../assets/frontend/assets/img/backgrounds/6.jpg"
-              alt="" />
+            <img class="w-100" src="../../../assets/frontend/assets/img/backgrounds/6.jpg" alt="" />
           </div>
         </div>
       </div>
@@ -108,15 +94,9 @@
         <div class="card">
           <h5 class="card-header">Video</h5>
           <div class="card-body">
-            <video
-              class="w-100"
-              poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
-              id="plyr-video-player"
-              playsinline
-              controls>
-              <source
-                src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
-                type="video/mp4" />
+            <video class="w-100" poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
+              id="plyr-video-player" playsinline controls>
+              <source src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4" type="video/mp4" />
             </video>
           </div>
         </div>
@@ -126,10 +106,7 @@
         <div class="card">
           <h5 class="card-header">Video</h5>
           <div class="card-body">
-            <img
-              class="h-50 w-100"
-              src="../../../assets/frontend/assets/img/elements/1.jpg"
-              alt="" />
+            <img class="h-50 w-100" src="../../../assets/frontend/assets/img/elements/1.jpg" alt="" />
           </div>
         </div>
       </div>
@@ -138,10 +115,7 @@
         <div class="card">
           <h5 class="card-header">Video</h5>
           <div class="card-body">
-            <img
-              class="h-50 w-100"
-              src="../../../assets/frontend/assets/img/ecommerce-images/product-10.png"
-              alt="" />
+            <img class="h-50 w-100" src="../../../assets/frontend/assets/img/ecommerce-images/product-10.png" alt="" />
           </div>
         </div>
       </div>
@@ -169,11 +143,7 @@
           <div class="modal-header border-bottom">
             <!-- comment title -->
             <h5 class="modal-title mb-2" id="modalScrollableTitle">Comments</h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"></button>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
             <!-- Repeated comment elements here -->
@@ -181,9 +151,7 @@
               <div class="card-header">
                 <div class="d-flex">
                   <div class="avatar me-3">
-                    <img
-                      src="../../../assets/frontend/assets/img/avatars/1.png"
-                      class="h-auto rounded-circle" />
+                    <img src="../../../assets/frontend/assets/img/avatars/1.png" class="h-auto rounded-circle" />
                   </div>
                   <div>
                     <h5>Alex leman</h5>
@@ -200,14 +168,10 @@
               </div>
 
               <div class="card-body">
-                <video
-                  class="w-100 rounded"
-                  poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
-                  id="plyr-video-player"
-                  playsinline
-                  controls>
-                  <source
-                    src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
+                <video class="w-100 rounded"
+                  poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg" id="plyr-video-player"
+                  playsinline controls>
+                  <source src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
                     type="video/mp4" />
                 </video>
               </div>
@@ -217,10 +181,7 @@
                     <i class="fa-regular fa-heart me-1 fs-4"></i>
                     <span style="font-size: 12px">12k</span>
                   </button>
-                  <button
-                    class="btn border-0"
-                    data-bs-toggle="modal"
-                    data-bs-target="#comment">
+                  <button class="btn border-0" data-bs-toggle="modal" data-bs-target="#comment">
                     <i class="fa-regular fa-comment me-1 fs-4"></i>
                     <span style="font-size: 12px">12k</span>
                   </button>
@@ -233,9 +194,7 @@
               <!-- comment -->
               <div class="d-flex">
                 <div class="avatar me-3">
-                  <img
-                    src="../../../assets/frontend/assets/img/avatars/1.png"
-                    class="rounded-circle cmt-img" />
+                  <img src="../../../assets/frontend/assets/img/avatars/1.png" class="rounded-circle cmt-img" />
                 </div>
                 <div>
                   <p class="card p-3">
@@ -253,10 +212,7 @@
                   <i class="fa-regular fa-heart me-1 fs-6"></i>
                   <span style="font-size: 12px">12k</span>
                 </button>
-                <button
-                  class="btn border-0"
-                  data-bs-toggle="modal"
-                  data-bs-target="#comment">
+                <button class="btn border-0" data-bs-toggle="modal" data-bs-target="#comment">
                   <i class="fa-regular fa-comment me-1 fs-6"></i>
                   <span style="font-size: 12px">Reply</span>
                 </button>
@@ -264,17 +220,11 @@
               <!-- comment -->
             </div>
           </div>
-          <div
-            class="modal-cmt-footer border-top d-flex align-items-center justify-content-around">
+          <div class="modal-cmt-footer border-top d-flex align-items-center justify-content-around">
             <div class="avatar me-1">
-              <img
-                src="../../../assets/frontend/assets/img/avatars/1.png"
-                class="h-auto rounded-circle" />
+              <img src="../../../assets/frontend/assets/img/avatars/1.png" class="h-auto rounded-circle" />
             </div>
-            <input
-              class="form-control mt-2 rounded-pill w-75"
-              placeholder="Coment as ashraful"
-              type="text" />
+            <input class="form-control mt-2 rounded-pill w-75" placeholder="Coment as ashraful" type="text" />
             <button type="submit" class="btn">
               <i class="fa-solid fa-paper-plane me-1"></i>Send
             </button>
@@ -282,60 +232,51 @@
         </div>
       </div>
     </div>
-    
+
     <div class="modal fade" id="NewPost" tabindex="-1" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-scrollable" role="document">
+      <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header border-bottom">
             <!-- comment title -->
-            <h5 class="modal-title mb-2" id="modalScrollableTitle">Comments</h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"></button>
+            <h5 class="modal-title mb-2 mx-auto fs-4 fw-bold" id="modalScrollableTitle">Create Post</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <!-- Repeated comment elements here -->
-            <div class="comment-body">
-              <div class="card-header">
-                <div class="d-flex">
-                  <div class="avatar me-3">
-                    <img
-                      src="../../../assets/frontend/assets/img/avatars/1.png"
-                      class="h-auto rounded-circle" />
-                  </div>
-                  
+            <div class="card-header border-bottom">
+              <div class="d-flex">
+                <div class="avatar me-3">
+                  <img src="../../../assets/frontend/assets/img/avatars/1.png" class="h-auto rounded-circle" />
                 </div>
-                
+                <!-- post name -->
+                <span class="fw-bold fs-6">Ashraful Islam Sahim</span>
               </div>
-
-              <div class="card-body">
-              
-              </div>
-              <div class="card-footer border-bottom mb-5">
-                
-              </div>
-              <!-- comment -->
-              
-             
-              <!-- comment -->
+              <!-- post area -->
+              <textarea class="form-control border-none post-area shadow-none" placeholder="What's new?" name=""
+                id=""></textarea>
+              <!-- dropdown modal -->
+              <div class="col-md-6 mb-4">
+                          <label for="selectpickerIcons" class="form-label">Icons</label>
+                          <select
+                            class="selectpicker w-100 show-tick"
+                            id="selectpickerIcons"
+                            data-icon-base="ti"
+                            data-tick-icon="ti-check"
+                            data-style="btn-default">
+                            <option data-icon="ti ti-brand-instagram">Instagram</option>
+                            <option data-icon="ti ti-brand-pinterest">Pinterest</option>
+                            <option data-icon="ti ti-brand-twitch">Twitch</option>
+                          </select>
+                        </div>
             </div>
-          </div>
-          <div
-            class="modal-cmt-footer border-top d-flex align-items-center justify-content-around">
-            <div class="avatar me-1">
-              <img
-                src="../../../assets/frontend/assets/img/avatars/1.png"
-                class="h-auto rounded-circle" />
+            <!-- body -->
+            <div class="card-body">
+              <!-- Hidden file input -->
+              <input type="file" class="d-none" id="file-upload">
+              <!-- Custom label with an image icon -->
+              <label for="file-upload" class="custom-file-label mt-2">
+                <span><i class="fa-solid fa-images fs-4"></i></span>
+              </label>
             </div>
-            <input
-              class="form-control mt-2 rounded-pill w-75"
-              placeholder="Coment as ashraful"
-              type="text" />
-            <button type="submit" class="btn">
-              <i class="fa-solid fa-paper-plane me-1"></i>Send
-            </button>
           </div>
         </div>
       </div>
@@ -344,10 +285,10 @@
 
 
   <div class="post-button-container">
-    <button class="post-button btn-primary"  data-bs-toggle="modal"
-    data-bs-target="#NewPost"><i class="fa-solid fa-feather-pointed"></i></button>
+    <button class="post-button btn-primary" data-bs-toggle="modal" data-bs-target="#NewPost"><i
+        class="fa-solid fa-feather-pointed"></i></button>
   </div>
- 
+
 </template>
 
 <!-- <script>
@@ -365,6 +306,23 @@ export default {
 };
 </script> -->
 <style scoped>
+.post-area {
+  resize: none;
+  margin-left: 40px;
+  background: transparent;
+  width: 90%;
+  height: 120px;
+}
+
+textarea::-webkit-scrollbar {
+  width: 5px !important;
+}
+
+.post-area::placeholder {
+  font-size: 20px !important;
+  padding-top: 0 !important;
+}
+
 .modal-cmt-footer {
   padding: 0.25rem 1.5rem 1.25rem;
 }
@@ -372,10 +330,12 @@ export default {
 .like {
   margin-top: -10px !important;
 }
+
 .cmt-img {
   width: 45px !important;
   height: 45px !important;
 }
+
 .timer {
   display: block;
   margin-top: -15px;
@@ -403,18 +363,23 @@ export default {
 
 /* CSS to hide scrollbar */
 .no-scrollbar {
-  overflow: hidden; /* Prevents scrollbars */
+  overflow: hidden;
+  /* Prevents scrollbars */
 }
 
 /* Optional: If you still need scrollable content but hidden scrollbar */
 .no-scrollbar {
-  overflow: auto; /* Enables scrolling but hides the scrollbar */
-  -ms-overflow-style: none; /* For IE and Edge */
-  scrollbar-width: none; /* For Firefox */
+  overflow: auto;
+  /* Enables scrolling but hides the scrollbar */
+  -ms-overflow-style: none;
+  /* For IE and Edge */
+  scrollbar-width: none;
+  /* For Firefox */
 }
 
 .no-scrollbar::-webkit-scrollbar {
-  display: none; /* For Chrome, Safari, and other WebKit browsers */
+  display: none;
+  /* For Chrome, Safari, and other WebKit browsers */
 }
 
 
@@ -440,5 +405,5 @@ export default {
 .post-button:hover {
   transform: rotate(360deg);
 }
-
 </style>
+
